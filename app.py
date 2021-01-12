538110b243801d808ab819535f721f635b56e560
Lennart Obst <hardyspielt@googlemail.com>
2021-01-12 13:50:06 +0100

    posts are unnecessary now

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/search', methods=['GET', 'POST'])
+@app.route('/search', methods=['GET'])
 def search():
     if request.method == 'GET':
         if not request.args:
