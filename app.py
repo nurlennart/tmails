5394e8e290c366afe5432dbf1b34e59b5f4cd23d
Lennart Obst <hardyspielt@googlemail.com>
2021-01-12 13:29:12 +0100

    initial

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask import render_template, request, redirect
+import json
+import sqlite3
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    if request.method == 'GET':
+        if not request.args:
+            return(redirect('/', 302))
+        elif request.args['query'] == '':
+            return(redirect('/', 302))
+        else:
+            conn = sqlite3.connect('teachers.db')
+            c = conn.cursor()
+            search = request.args['query']
+            c.execute("SELECT name, address FROM teachers WHERE name LIKE ? OR address LIKE ?", (f'%{search}%', f'%{search}%'))
+            conn.commit()
+            data = c.fetchall()
+            return render_template('index.html', teachers=data)
+    else:
+        return(redirect('/', 302))
+        
